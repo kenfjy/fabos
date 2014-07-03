cd6e0d893b546a99820e69a0620bb26941b0cbc2
Ken Fujiyoshi <kenfjy@gmail.com>
2014-07-03 19:45:46 +0900

    added git ignore, modified flow

--- tornado_sample.py
+++ tornado_sample.py
@@ -3,11 +3,19 @@ import tornado.web
 import tornado.options
 import os, uuid, cups
 
+# cups object
 c = None
-f = None
+# printer name
 p = None
+# uploaded file path
+u_f = None
+# middle file path
+m_f = None
+# print file path
+p_f = None
 
 __UPLOADS__ = "static/uploads/"
+__TMP__ = "static/tmp"
 
 class Application(tornado.web.Application):
     def __init__(self):
@@ -15,7 +23,6 @@ class Application(tornado.web.Application):
             (r"/", FileHandler),
             (r"/printer.*", PrinterHandler),
             (r"/confirm.*", ConfirmHandler),
-            (r"/printfile.*", PrintFileHandler),
             (r"/uploads/^(.*)",tornado.web.StaticFileHandler, {"path": "./uploads"},),
         ]
         settings = dict(
@@ -27,26 +34,28 @@ class Application(tornado.web.Application):
 # handles files
 class FileHandler(tornado.web.RequestHandler):
     def get(self):
-        global f;
-        self.render("upload.html", filename = f)
+        global u_f;
+        self.render("upload.html", filename = u_f)
+        
     def post(self):
-        global f;
-        self.render("upload.html", filename = f)
+        global u_f;
+        self.render("upload.html", filename = u_f)
 
 # handles printer check
 class PrinterHandler(tornado.web.RequestHandler):
     def get(self):
-        global c, f, p
-        if f is None: 
+        global c, p, u_f
+        if u_f is None: 
             self.redirect("/")
-        else:
-            if c is None:
-                c = cups.Connection()
-            printers = c.getPrinters()
-            
-            self.render("printer.html", printers=printers, filename = f, sel_printer=p)
+
+        if c is None:
+            c = cups.Connection()
+        printers = c.getPrinters()
+        
+        self.render("printer.html", printers=printers, sel_printer=p, filename = u_f)
+
     def post(self):
-        global c, f, p
+        global c, p, u_f
         fileinfo = self.request.files['up_file'][0]
         print "fileinfo is ", fileinfo
         fname = fileinfo['filename']
@@ -54,33 +63,36 @@ class PrinterHandler(tornado.web.RequestHandler):
         cname = str(uuid.uuid4()) + extn
         fh = open(__UPLOADS__ + cname, 'w')
         fh.write(fileinfo['body'])
-        f = cname
+        u_f = cname
 
-        fpath = os.path.join(os.path.dirname(__file__), __UPLOADS__ + f)
+        fpath = os.path.join(os.path.dirname(__file__), __UPLOADS__ + u_f)
         print fpath
 
         if c is None:
             c = cups.Connection()
         printers = c.getPrinters()
 
-        self.render("printer.html", printers=printers, filename = f, sel_printer=p)
+        self.render("printer.html", printers=printers, sel_printer=p, filename = u_f)
 
 # handles printing confirmation
 class ConfirmHandler(tornado.web.RequestHandler):
     def get(self):
-        global c, f, p
-        if f is None:
+        global c, p, u_f
+        if u_f is None:
             self.redirect("/")
         if c is None:
             self.redirect("/printer")
         if p is None:
             self.redirect("/printer")
 
-        self.write("you have chosen " + p)
+        fpath = os.path.join(os.path.dirname(__file__), __UPLOADS__ + u_f)
+        print fpath
+
+        c.printFile(p, "static/uploads/" + u_f, "rml milling", {})
 
     def post(self):
-        global c, f, p
-        if f is None:
+        global c, p, u_f
+        if u_f is None:
             self.redirect("/")
         if c is None:
             self.redirect("/printer")
@@ -89,31 +101,9 @@ class ConfirmHandler(tornado.web.RequestHandler):
         if p is None:
             self.redirect("/printer")
 
-        self.write("you have chosen " + p)
+        fpath = os.path.join(os.path.dirname(__file__), __UPLOADS__ + u_f)
 
-# handles printing confirmation
-class PrintFileHandler(tornado.web.RequestHandler):
-    def get(self):
-        global c, f, p
-        if f is None:
-            self.redirect("/")
-        if c is None:
-            self.redirect("/printer")
-        if p is None:
-            self.redirect("/printer")
-
-        c.printFile(p, "static/uploads/" + f, "rml milling", {})
-
-    def post(self):
-        global c, f, p
-        if f is None:
-            self.redirect("/")
-        if c is None:
-            self.redirect("/printer")
-        if p is None:
-            self.redirect("/printer")
-
-        c.printFile(p, "static/uploads/" + f, "rml milling", {})
+        self.render("confirm.html", filepath=fpath, filename=u_f, printer = p)
 
 # class PrintHandler(tornado.web.RequestHandler):
 #     def get(self):
@@ -127,9 +117,15 @@ def main():
     # test for directory existance
     uppath = __UPLOADS__ + "sample.txt"
     uppath = os.path.dirname(uppath)
-    print "PATH: " + uppath
     if not os.path.exists(uppath):
+        print "making directories at " + uppath
         os.makedirs(uppath)
+
+    temppath = __TMP__ + "sample.txt"
+    temppath = os.path.dirname(temppath)
+    if not os.path.exists(temppath):
+        print "making directories at " + temppath
+        os.makedirs(temppath)
 
     cups.setServer("localhost")
     application = Application()
